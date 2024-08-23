d2c715ccd92c25cb51eab2e055887fdbb2eaa3ef
Vinicius Andrade <fabioviniciusbarbosa@hotmail.com>
2024-08-23 09:20:05 -0300

    Rota para pegar todos os livros agora conta com filtros e paginação

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -14,9 +14,29 @@ export const createBook = async (req, res) => {
 };
 
 export const getAllBooks = async (req, res) => {
+  const { page = 1, limit = 10, author, category, publisher } = req.query;
+
+  // Filtros Dinâmicos
+  const where = {};
+  if (author) where.autorId = Number(author);
+  if (category) where.categoriaId = Number(category);
+  if (publisher) where.editoraId = Number(publisher);
+
   try {
-    const books = await prisma.livro.findMany();
-    return res.status(200).json(books);
+    const books = await prisma.livro.findMany({
+      where,
+      take: Number(limit),
+      skip: (Number(page) - 1) * Number(limit),
+    });
+
+    // serve para contar quantos livros existem no banco de dados
+    const count = await prisma.livro.count({ where });
+
+    return res.status(200).json({
+      books,
+      totalPages: Math.ceil(count / limit),
+      currentPage: Number(page),
+    });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ message: "something went wrong, please try again later." });
--- src/routers/bookRoutes.js
+++ src/routers/bookRoutes.js
@@ -9,13 +9,24 @@ import controllerAccess from "../middlewares/controllerAccess.js";
 
 // Criar um livro (Funcionario)
 routes.post("/create", authRequired, controllerAccess(), bookController.createBook);
+
 // Mostrar todos os livros (Todos os usuarios)
 routes.get("/all", bookController.getAllBooks);
+
 // Mostar um livro especifico (Todos os usuarios)
 routes.get("/show/:id", bookController.getShowBook);
+
 // Atualizar os dados de um livro (Funcionario)
 routes.put("/update/:id", authRequired, controllerAccess(), bookController.updateBook);
+
 // Deletar um livro (Funcionario)
 routes.delete("/delete/:id", authRequired, controllerAccess(), bookController.deleteBook);
 
 export default routes;
+
+/*
+Formas de usar o endpoint /all
+
+GET /book/all?page=1&limit=30
+GET /book/all?author=2&category=2&publisher=3&page=2&limit=5
+*/
